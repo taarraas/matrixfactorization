b0d90c322d2032e2def24edadaac4c1e461de636
taras <taras@192.168.0.8>
2013-08-15 11:07:34 -0700

    extracting 2 and 3 words

--- src/main/java/knu/univ/lingvo/up/ThreeWordsExtractor.java
+++ src/main/java/knu/univ/lingvo/up/ThreeWordsExtractor.java
@@ -4,10 +4,83 @@
  */
 package knu.univ.lingvo.up;
 
+import java.util.ArrayList;
+import knu.univ.lingvo.wikiner.NER;
+
 /**
  *
  * @author taras
  */
 public class ThreeWordsExtractor {
-    
+
+    public static ArrayList<String[]> getThreeWordsB(SpaceElement se, SpaceElement ser) {
+        ArrayList<String[]> twoWordsList = new ArrayList();
+        if (ser.getType() == SpaceElement.Type.B) {
+            String[] thisLevel = new String[6];
+            if (se.getLeft() instanceof SpaceElement) {
+                thisLevel[0] = ((SpaceElement) se.getLeft()).getWord();
+                thisLevel[1] = ((SpaceElement) se.getLeft()).getTag();
+            } else {
+                thisLevel[0] = (String) se.getLeft();
+                thisLevel[1] = "unk";
+            }
+
+            if (ser.getLeft() instanceof SpaceElement) {
+                thisLevel[2] = ((SpaceElement) ser.getLeft()).getWord();
+                thisLevel[3] = ((SpaceElement) ser.getLeft()).getTag();
+            } else {
+                thisLevel[2] = (String) ser.getLeft();
+                thisLevel[3] = "unk";
+            }
+
+            if (ser.getRight() instanceof SpaceElement) {
+                thisLevel[4] = ((SpaceElement) ser.getRight()).getWord();
+                thisLevel[5] = ((SpaceElement) ser.getRight()).getTag();
+            } else {
+                thisLevel[4] = (String) ser.getRight();
+                thisLevel[5] = "unk";
+            }
+
+            twoWordsList.add(thisLevel);
+        }
+        if (ser.getLeft() instanceof SpaceElement) {
+            twoWordsList.addAll(getThreeWordsB(se, (SpaceElement) ser.getLeft()));
+        }
+
+        if (ser.getRight() instanceof SpaceElement) {
+            twoWordsList.addAll(getThreeWordsB(se, (SpaceElement) ser.getRight()));
+        }
+        return twoWordsList;
+    }
+
+    public static ArrayList<String[]> getThreeWords(SpaceElement se) {
+        ArrayList<String[]> twoWordsList = new ArrayList();
+        if (se.getType() == SpaceElement.Type.A) {
+
+            if (se.getRight() instanceof SpaceElement) {
+                twoWordsList.addAll(getThreeWordsB(se, (SpaceElement) se.getRight()));
+            }
+        }
+
+        if (se.getLeft() instanceof SpaceElement) {
+            twoWordsList.addAll(getThreeWords((SpaceElement) se.getLeft()));
+        }
+
+        if (se.getRight() instanceof SpaceElement) {
+            twoWordsList.addAll(getThreeWords((SpaceElement) se.getRight()));
+        }
+        return twoWordsList;
+    }
+
+    public static void main(String argv[]) {
+        SpaceForSentenceCreator sfsc = new SpaceForSentenceCreator();
+        SpaceElement space = sfsc.getSpace("The strongest rain ever recorded in India shut down the financial hub of Mumbai, snapped communication lines, closed airports and forced thousands of people to sleep in their offices.");
+        ArrayList<String[]> two = getThreeWords(space);
+        for (String[] strings : two) {
+            for (String string : strings) {
+                System.out.print(string + " ");
+            }
+            System.out.println("");
+        }
+    }
 }
--- src/main/java/knu/univ/lingvo/up/TwoWordsExtractor.java
+++ src/main/java/knu/univ/lingvo/up/TwoWordsExtractor.java
@@ -12,36 +12,45 @@ import knu.univ.lingvo.wikiner.NER;
  * @author taras
  */
 public class TwoWordsExtractor {
+
     public static ArrayList<String[]> getTwoWords(SpaceElement se) {
         ArrayList<String[]> twoWordsList = new ArrayList();
-        
-        
-        String[] thisLevel = new String[4];
-        if (se.getLeft() instanceof SpaceElement) {
-            thisLevel[0] = ((SpaceElement) se.getLeft()).getWord();
-        } else {
-            thisLevel[0] = (String) se.getLeft();
-        }
-        if (se.getRight() instanceof SpaceElement) {
-            thisLevel[1] = ((SpaceElement) se.getRight()).getWord();
-        } else {
-            thisLevel[1] = (String) se.getRight();
-        }
-        
-        thisLevel[2] = se.getTag();
-        thisLevel[3] = se.getType().toString();
-        
-        twoWordsList.add(thisLevel);
-        
-        if (se.getLeft() instanceof SpaceElement)
-            twoWordsList.addAll(getTwoWords((SpaceElement)se.getLeft()));
 
-        if (se.getRight() instanceof SpaceElement)
-            twoWordsList.addAll(getTwoWords((SpaceElement)se.getRight()));
-        
+
+        if (se.getType() == SpaceElement.Type.AB) {
+            String[] thisLevel = new String[6];
+            if (se.getLeft() instanceof SpaceElement) {
+                thisLevel[0] = ((SpaceElement) se.getLeft()).getWord();
+                thisLevel[1] = ((SpaceElement) se.getLeft()).getTag();
+            } else {
+                thisLevel[0] = (String) se.getLeft();
+                thisLevel[1] = "unk";
+            }
+            if (se.getRight() instanceof SpaceElement) {
+                thisLevel[2] = ((SpaceElement) se.getRight()).getWord();
+                thisLevel[3] = ((SpaceElement) se.getRight()).getTag();
+            } else {
+                thisLevel[2] = (String) se.getRight();
+                thisLevel[3] = "unk";
+            }
+
+            thisLevel[4] = se.getWord();
+            thisLevel[5] = se.getTag();
+
+            twoWordsList.add(thisLevel);
+        }
+
+        if (se.getLeft() instanceof SpaceElement) {
+            twoWordsList.addAll(getTwoWords((SpaceElement) se.getLeft()));
+        }
+
+        if (se.getRight() instanceof SpaceElement) {
+            twoWordsList.addAll(getTwoWords((SpaceElement) se.getRight()));
+        }
+
         return twoWordsList;
     }
-    
+
     public static void main(String argv[]) {
         SpaceForSentenceCreator sfsc = new SpaceForSentenceCreator();
         SpaceElement space = sfsc.getSpace("The strongest rain ever recorded in India shut down the financial hub of Mumbai, snapped communication lines, closed airports and forced thousands of people to sleep in their offices.");
